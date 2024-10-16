d242b6092e77ae673339cdaa120f63a2afbda1aa
hoorieh.23 <hoorieh.23@gmail.com>
2024-10-16 14:57:23 +0200

    Design Changes

--- MatRecept/Form1.Designer.cs
+++ MatRecept/Form1.Designer.cs
@@ -41,6 +41,9 @@
             buttonAdmin = new Button();
             textBox1 = new TextBox();
             buttonSearch = new Button();
+            buttonEdit = new Button();
+            buttonCreate = new Button();
+            buttonRemove = new Button();
             SuspendLayout();
             // 
             // listBoxMeat
@@ -50,6 +53,7 @@
             listBoxMeat.Name = "listBoxMeat";
             listBoxMeat.Size = new Size(123, 204);
             listBoxMeat.TabIndex = 0;
+            listBoxMeat.SelectedIndexChanged += listBoxMeat_SelectedIndexChanged;
             // 
             // listBoxFish
             // 
@@ -58,6 +62,7 @@
             listBoxFish.Name = "listBoxFish";
             listBoxFish.Size = new Size(123, 204);
             listBoxFish.TabIndex = 1;
+            listBoxFish.SelectedIndexChanged += listBoxFish_SelectedIndexChanged;
             // 
             // listBoxSalad
             // 
@@ -66,6 +71,7 @@
             listBoxSalad.Name = "listBoxSalad";
             listBoxSalad.Size = new Size(123, 204);
             listBoxSalad.TabIndex = 2;
+            listBoxSalad.SelectedIndexChanged += listBoxSalad_SelectedIndexChanged;
             // 
             // listBoxSoup
             // 
@@ -74,15 +80,16 @@
             listBoxSoup.Name = "listBoxSoup";
             listBoxSoup.Size = new Size(123, 204);
             listBoxSoup.TabIndex = 3;
+            listBoxSoup.SelectedIndexChanged += listBoxSoup_SelectedIndexChanged;
             // 
             // listBoxDessert
             // 
             listBoxDessert.FormattingEnabled = true;
-            listBoxDessert.Location = new Point(585, 84);
+            listBoxDessert.Location = new Point(585, 95);
             listBoxDessert.Name = "listBoxDessert";
             listBoxDessert.Size = new Size(123, 204);
             listBoxDessert.TabIndex = 4;
-          //  listBoxDessert.SelectedIndexChanged += listBoxDessert_SelectedIndexChanged;
+            listBoxDessert.SelectedIndexChanged += listBoxDessert_SelectedIndexChanged;
             // 
             // label1
             // 
@@ -92,6 +99,7 @@
             label1.Size = new Size(43, 20);
             label1.TabIndex = 5;
             label1.Text = "Meat";
+            label1.Click += label1_Click;
             // 
             // label2
             // 
@@ -101,6 +109,7 @@
             label2.Size = new Size(34, 20);
             label2.TabIndex = 6;
             label2.Text = "Fish";
+            label2.Click += label2_Click;
             // 
             // label3
             // 
@@ -110,6 +119,7 @@
             label3.Size = new Size(46, 20);
             label3.TabIndex = 7;
             label3.Text = "Salad";
+            label3.Click += label3_Click;
             // 
             // label4
             // 
@@ -119,46 +129,88 @@
             label4.Size = new Size(43, 20);
             label4.TabIndex = 8;
             label4.Text = "Soup";
+            label4.Click += label4_Click;
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(585, 50);
+            label5.Location = new Point(585, 62);
             label5.Name = "label5";
             label5.Size = new Size(58, 20);
             label5.TabIndex = 9;
             label5.Text = "Dessert";
+            label5.Click += label5_Click;
             // 
             // buttonAdmin
             // 
+            buttonAdmin.BackColor = Color.Chocolate;
             buttonAdmin.Location = new Point(12, 12);
             buttonAdmin.Name = "buttonAdmin";
             buttonAdmin.Size = new Size(126, 29);
             buttonAdmin.TabIndex = 10;
             buttonAdmin.Text = "Admin";
-            buttonAdmin.UseVisualStyleBackColor = true;
+            buttonAdmin.UseVisualStyleBackColor = false;
+            buttonAdmin.Click += buttonAdmin_Click;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(196, 360);
+            textBox1.Location = new Point(137, 329);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(205, 27);
             textBox1.TabIndex = 11;
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // buttonSearch
             // 
-            buttonSearch.Location = new Point(62, 360);
+            buttonSearch.BackColor = Color.PaleGreen;
+            buttonSearch.Location = new Point(15, 329);
             buttonSearch.Name = "buttonSearch";
             buttonSearch.Size = new Size(109, 32);
             buttonSearch.TabIndex = 12;
             buttonSearch.Text = "Search";
-            buttonSearch.UseVisualStyleBackColor = true;
+            buttonSearch.UseVisualStyleBackColor = false;
+            // 
+            // buttonEdit
+            // 
+            buttonEdit.BackColor = Color.LemonChiffon;
+            buttonEdit.Location = new Point(12, 376);
+            buttonEdit.Name = "buttonEdit";
+            buttonEdit.Size = new Size(112, 27);
+            buttonEdit.TabIndex = 13;
+            buttonEdit.Text = "Edit";
+            buttonEdit.UseVisualStyleBackColor = false;
+            buttonEdit.Click += buttonEdit_Click;
+            // 
+            // buttonCreate
+            // 
+            buttonCreate.BackColor = Color.LightCyan;
+            buttonCreate.Location = new Point(163, 376);
+            buttonCreate.Name = "buttonCreate";
+            buttonCreate.Size = new Size(112, 27);
+            buttonCreate.TabIndex = 14;
+            buttonCreate.Text = "Create";
+            buttonCreate.UseVisualStyleBackColor = false;
+            buttonCreate.Click += buttonCreate_Click;
+            // 
+            // buttonRemove
+            // 
+            buttonRemove.BackColor = Color.IndianRed;
+            buttonRemove.Location = new Point(307, 376);
+            buttonRemove.Name = "buttonRemove";
+            buttonRemove.Size = new Size(112, 27);
+            buttonRemove.TabIndex = 15;
+            buttonRemove.Text = "Remove";
+            buttonRemove.UseVisualStyleBackColor = false;
+            buttonRemove.Click += buttonRemove_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(713, 433);
+            Controls.Add(buttonRemove);
+            Controls.Add(buttonCreate);
+            Controls.Add(buttonEdit);
             Controls.Add(buttonSearch);
             Controls.Add(textBox1);
             Controls.Add(buttonAdmin);
@@ -193,5 +245,8 @@
         private Button buttonAdmin;
         private TextBox textBox1;
         private Button buttonSearch;
+        private Button buttonEdit;
+        private Button buttonCreate;
+        private Button buttonRemove;
     }
 }
