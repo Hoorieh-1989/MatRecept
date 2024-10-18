a32a7c8221bc93b38ccd2f8f634fe711da3ca3e2
Milton Raninen Flyckt <Milton.raninen1@outlook.com>
2024-10-18 11:51:43 +0200

    Admin Buttons Funcunality Done

--- MatRecept/Form1.cs
+++ MatRecept/Form1.cs
@@ -2,19 +2,32 @@ namespace MatRecept
 {
     public partial class Form1 : Form
     {
-
+        
 
         public Form1()
         {
             InitializeComponent();
+
+
             buttonCreate.Visible = false;
             buttonEdit.Visible = false;
             buttonRemove.Visible = false;
+
+
+        }
+
+        public void ShowAdminButtons()
+        {
+            buttonCreate.Visible = true;
+            buttonEdit.Visible = true;
+            buttonRemove.Visible = true;
         }
 
         private void buttonAdmin_Click(object sender, EventArgs e)
         {
-
+            LoginForum loginForum = new LoginForum(this);
+            loginForum.ShowDialog();
         }
+
     }
 }
---
+++ MatRecept/LoginForum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatRecept
+{
+    public partial class LoginForum : Form
+    {
+
+        private Form1 mainform;
+
+        public LoginForum(Form1 form1)
+        {
+            InitializeComponent();
+            this.mainform = form1;
+        }
+
+        private void buttonSignIn_Click(object sender, EventArgs e)
+        {
+            string username = textBoxUsername.Text;
+            string password = textBoxPassword.Text;
+
+            if (username == "admin" && password == "1234")
+            {
+                MessageBox.Show("Grattis du är nu Inloggad som Admin");
+                mainform.ShowAdminButtons();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Felaktig inloggning");
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
